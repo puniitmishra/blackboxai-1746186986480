3875a2c4c8c13f52a3675e74336e782805254c94
Punit Mishra <94481120+puniitmishra@users.noreply.github.com>
2025-05-02 17:26:50 +0530

    Add backend/leads/views.py 1746187010127

---
+++ backend/leads/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Lead
+from .serializers import LeadSerializer
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
